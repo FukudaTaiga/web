98c05dfa95156d123b6759cb92adf6a80e7691f0
FukudaTaiga <fuku.taiga.0119@gmail.com>
2021-03-01 20:50:05 +0900

    2021-03-01 19:54:03

--- _inc/comment.php
+++ _inc/comment.php
@@ -1,3 +1,18 @@
+<?php
+  define('FILENAME', "{$path}comments.txt");
+  date_default_timezone_set('Asia/Tokyo');
+
+  //initialize variables
+  $file_handle = null;
+  $date = null;
+  $data = null;
+  $split_data = null;
+  $msg = array();
+  $msg_array = array();
+?>
+
+
+<!-- comment input form -->
 <form class="comment" method="post">
   <div class="comment_name_div">
     <label for="comment_name">
@@ -13,23 +28,65 @@
 </form>
 
 <?php
-  define('FILENAME', "{$path}comments.txt");
+if (!empty($_POST['submit']) && !empty($_POST['comment_name']) && !empty($_POST['comment_msg'])) {
+  if ($file_handle = fopen(FILENAME, "a")) {
+    $date = date("Y-m-d H:i:s");
+    $data =
+    "'".$_POST['comment_name'].
+    "','".$_POST['comment_msg'].
+    "','".$date."'\n";
 
-  date_default_timezone_set('Asia/Tokyo');
+    fwrite($file_handle, $data);
 
-  var_dump($_POST);
-
-  if (!empty($_POST['submit'])) {
-    if ($file_handle = fopen(FILENAME, "a")) {
-      $now_date = date("Y-m-d H:i:s");
-      $data =
-      "'".$_POST['comment_name'].
-      "','".$_POST['comment_msg'].
-      "','".$now_date."'\n";
-
-      fwrite($file_handle, $data);
-
-      fclose($file_handle);
-    }
+    fclose($file_handle);
   }
+}
 ?>
+<!-- comment input form -->
+
+<!-- comment section -->
+<?php
+if ($file_handle = fopen(FILENAME, "r")) {
+  while ($data = fgets($file_handle)) {
+    $split_data = preg_split('/\'/', $data);
+    /*$split_data = array(7) {
+    [0]=> string(0) ""
+    [1]=> string(6) comment_name
+    [2]=> string(1) ","
+    [3]=> string(4) comment_msg
+    [4]=> string(1) ","
+    [5]=> string(19) $now
+    [6]=> string(1) " " }
+    */
+    $msg = array(
+      'comment_name' => $split_data[1],
+      'msg' => $split_data[3],
+      'date' => $split_data[5]
+    );
+    array_unshift($msg_array, $msg); //配列の先頭から追加 <=> push
+  }
+
+
+  fclose($file_handle);
+}
+?>
+<aside id="comment_section">
+  <?php
+  if (!empty($msg_array)):
+    foreach ($msg_array as $value):
+  ?>
+  <article class="comment_per">
+    <div class="info">
+      <span class="writer_name"> <?php echo $value['comment_name']; ?> </span>
+      <time> <?php echo date('Y年m月d日 H:i', strtotime($value['date'])); ?> </time>
+    </div>
+    <p>
+      <?php echo $value['msg']; ?>
+    </p>
+  </article>
+  <?php
+    endforeach;
+  endif;
+  ?>
+
+</aside>
